c6654c7a6600016df6d482332fe5f3b51463b33a
cluser <cluser@Mac-mini-4.local>
2016-10-27 17:44:25 +0200

    Add install script for mac

---
+++ install.sh
@@ -0,0 +1,4 @@
+# Install script for windows
+# Assumes working dir is dotfiles/ and dotfiles resides in %USERPROFILE%
+echo source $HOME/dotfiles/vimrc > $HOME/.vimrc
+cp consolas-powerline-vim/CONSOLA-Powerline.ttf $HOME/Library/Fonts/
