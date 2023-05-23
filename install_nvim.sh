a2a1da3f117c9121e0e4aa29d48d4db4cfaa22cd
Squareys <squareys@googlemail.com>
2023-05-23 10:13:50 +0200

    Add install_nvim.sh
    Signed-off-by: Squareys <squareys@googlemail.com>

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,2 +1,7 @@
 mkdir ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
+
+echo source $HOME/dotfiles/vimrc > $HOME/.vimrc
+if [[ `uname -s` == "darwin" ]]; then
+    cp consolas-powerline-vim/CONSOLA-Powerline.ttf $HOME/Library/Fonts/
+fi
