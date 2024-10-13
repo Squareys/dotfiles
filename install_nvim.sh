e40cbb2c0dd9b06f9559948ecf994f59076e89b7
Squareys <squareys@googlemail.com>
2024-10-13 10:03:20 +0200

    Update registry values
    Signed-off-by: Squareys <squareys@googlemail.com>

--- install_nvim.sh
+++ install_nvim.sh
@@ -1,7 +1,6 @@
 mkdir ~/.config/nvim
 cp init.vim ~/.config/nvim/init.vim
 
-echo source $HOME/dotfiles/vimrc > $HOME/.vimrc
 if [[ `uname -s` == "darwin" ]]; then
     cp consolas-powerline-vim/CONSOLA-Powerline.ttf $HOME/Library/Fonts/
 fi
